18fe23b6711355a1058556072328f08500962103
forkandwait <forkandwait@gmail.com>
2011-04-25 14:02:14 -0700

    trying to work around MS silliness

--- linkify.sh
+++ linkify.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh  ## -*-mode:shell-script-*-
 
 ## script to linkify all the config scripts.  No "for loop" for control.
 
@@ -7,33 +7,19 @@ set -u
 
 case $OS in
 	Windows_NT)
-		# UGLY HACK FOR MISSING SYMLINK FUNCTIONALITY!!
-		LNOPTS=' -vb'
-
-		# DIRS
-		mkdir $HOME/.emacs.d
-		for EFILE in $HOME/.init/emacs.d/*.el; do
-			FF=$(basename $EFILE)
-			ln $LNOPTS $HOME/.init/emacs.d/$FF $HOME/.emacs.d/$FF
-		done
-
-		# FILES
-		ln $LNOPTS $HOME/.init/bashrc     $HOME/.bashrc
-		ln $LNOPTS $HOME/.init/inputrc    $HOME/.inputrc
-		ln $LNOPTS $HOME/.init/psqlrc     $HOME/.psqlrc
-		ln $LNOPTS $HOME/.init/octaverc   $HOME/.octaverc 
+		# WINDOWS LAMENESS: MSYS/ XP will just "cp" to emulate symlink (?!?!?!?)
+		LNOPTS=' -svb'
 		;;
 	*)
 		LNOPTS=' -svb'
-
-		# DIRS
-		ln $LNOPTS $HOME/.init/emacs.d    $HOME/.emacs.d
-
-		# FILES
-		ln $LNOPTS $HOME/.init/bashrc     $HOME/.bashrc
-		ln $LNOPTS $HOME/.init/inputrc    $HOME/.inputrc
-		ln $LNOPTS $HOME/.init/psqlrc     $HOME/.psqlrc
-		ln $LNOPTS $HOME/.init/octaverc   $HOME/.octaverc 
 		;;
 esac
 
+# DIRS
+ln $LNOPTS $HOME/.init/emacs.d    $HOME/.emacs.d
+
+# FILES
+ln $LNOPTS $HOME/.init/bashrc     $HOME/.bashrc
+ln $LNOPTS $HOME/.init/inputrc    $HOME/.inputrc
+ln $LNOPTS $HOME/.init/psqlrc     $HOME/.psqlrc
+ln $LNOPTS $HOME/.init/octaverc   $HOME/.octaverc 
